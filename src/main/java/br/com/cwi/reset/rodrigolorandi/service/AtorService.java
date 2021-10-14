f1288fdd174ba2845d1a5676144e3543bfab55e1
Rodrigo Lorandi <rodrigo_lorandi@hotmail.com>
2021-10-14 17:35:57 -0300

    create listarAtoresEmAtividade()

--- src/main/java/br/com/cwi/reset/rodrigolorandi/service/AtorService.java
+++ src/main/java/br/com/cwi/reset/rodrigolorandi/service/AtorService.java
@@ -6,6 +6,8 @@ import br.com.cwi.reset.rodrigolorandi.enums.StatusCarreira;
 import br.com.cwi.reset.rodrigolorandi.request.AtorRequest;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 public class AtorService {
 
@@ -20,12 +22,27 @@ public class AtorService {
     private StatusCarreira statusCarreira;
     private Integer anoInicioAtividade;
 
+    List<Ator> todosAtores = new ArrayList<>();
+
     public void criarAtor(AtorRequest atorRequest){
         Ator ator = new Ator(atorRequest.getNome(),
                 atorRequest.getDataNascimento(),
                 atorRequest.getStatusCarreira(),
                 atorRequest.getAnoInicioAtividade());
 
+        todosAtores.add(ator);
         fakeDatabase.persisteAtor(ator);
     }
+
+    public List<Ator> listarAtoresEmAtividade(String filtroNome){
+        for ( Ator ator : todosAtores){
+            if(ator.getStatusCarreira() == StatusCarreira.EM_ATIVIDADE){
+                System.out.println(ator);
+            }
+        }
+
+
+    }
+
+
 }
