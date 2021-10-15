cf5a6ac3eb2a7030bdb6f9c182dfb57f98142177
Rodrigo Lorandi <rodrigo_lorandi@hotmail.com>
2021-10-15 13:57:25 -0300

    Restart AtorService

--- src/main/java/br/com/cwi/reset/rodrigolorandi/service/AtorService.java
+++ src/main/java/br/com/cwi/reset/rodrigolorandi/service/AtorService.java
@@ -17,47 +17,7 @@ public class AtorService {
         this.fakeDatabase = fakeDatabase;
     }
 
-    private String nome;
-    private LocalDate dataNascimento;
-    private StatusCarreira statusCarreira;
-    private Integer anoInicioAtividade;
-
-    List<Ator> todosAtores = new ArrayList<>();
-
-    public void criarAtor(AtorRequest atorRequest) {
-        Ator ator = new Ator(atorRequest.getNome(),
-                atorRequest.getDataNascimento(),
-                atorRequest.getStatusCarreira(),
-                atorRequest.getAnoInicioAtividade());
-
-        todosAtores.add(ator);
-        fakeDatabase.persisteAtor(ator);
-    }
-
-    public List<Ator> listarAtoresEmAtividade(String filtroNome) {
-        List<Ator> atoresEmAtividade = new ArrayList<>();
-        for (Ator ator : todosAtores) {
-            if (filtroNome != null) {
-                if (ator.getNome().equals(filtroNome)) {
-                    if (ator.getStatusCarreira() == StatusCarreira.EM_ATIVIDADE) {
-                        atoresEmAtividade.add(ator);
-                    }
-                } else {
-                    System.out.println(String.format("Ator não encontrato com o filtro %s, favor informar outro filtro.", filtroNome));
-                }
-            } else {
-                if (ator.getStatusCarreira() == StatusCarreira.EM_ATIVIDADE) {
-                    atoresEmAtividade.add(ator);
-                }
-            }
-            if (todosAtores == null) {
-                System.out.println("Nenhum ator cadastrado, favor cadastar atores.");
-            }
-        }
-        return atoresEmAtividade;
-    }
-
-    
+    // Demais métodos da classe
 }
 
 
