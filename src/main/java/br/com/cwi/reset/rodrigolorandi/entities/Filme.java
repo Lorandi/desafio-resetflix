df600bab36cfcd0d0b01d81c970c9311d04a4553
Rodrigo Lorandi <rodrigo_lorandi@hotmail.com>
2021-10-16 07:50:20 -0300

    correct Filme

--- src/main/java/br/com/cwi/reset/rodrigolorandi/entities/Filme.java
+++ src/main/java/br/com/cwi/reset/rodrigolorandi/entities/Filme.java
@@ -5,8 +5,6 @@ import br.com.cwi.reset.rodrigolorandi.enums.Genero;
 import java.util.List;
 
 public class Filme {
-    private static Integer sequence = 0;
-
     private Integer id;
     private String nome;
     private Integer anoLancamento;
@@ -16,8 +14,8 @@ public class Filme {
     private List<PersonagemAtor> personagens;
     private String resumo;
 
-    public Filme(String nome, Integer anoLancamento, String capaFilme, List<Genero> generos, Diretor diretor, List<PersonagemAtor> personagens, String resumo) {
-        this.id = sequence++;
+    public Filme(Integer id, String nome, Integer anoLancamento, String capaFilme, List<Genero> generos, Diretor diretor, List<PersonagemAtor> personagens, String resumo) {
+        this.id = id;
         this.nome = nome;
         this.anoLancamento = anoLancamento;
         this.capaFilme = capaFilme;
